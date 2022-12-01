e92f0294d0d6c8b1c0cc4038347cba80dfcad5af
Mikael Ivarsson <mikael.ivarsson@lm.se>
2022-12-01 07:42:38 +0100

    split the elf

--- day01/solve.py
+++ day01/solve.py
@@ -1,5 +1,5 @@
-input = "../inputs/day01.txt"
+f = "../inputs/day01.txt"
 
-i = sorted([sum([int(l) for l in line.split()]) for line in open(input, "r").read().split("\n\n")], reverse=True)
+i = sorted([sum([int(c) for c in elf.split()]) for elf in open(f, "r").read().split("\n\n")], reverse=True)
 print("p1", i[0])
 print("p2", sum(i[:3]))
