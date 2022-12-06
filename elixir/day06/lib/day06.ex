a26921652a300de79f8b74e5969083ee9b4db3d2
Mikael Ivarsson <mikael.ivarsson@lm.se>
2022-12-06 15:21:17 +0100

    empty set check day06 elixir

--- elixir/day06/lib/day06.ex
+++ elixir/day06/lib/day06.ex
@@ -1,6 +1,6 @@
 defmodule Day06 do
   def solve(i, x, w) do
-    s = i |> Enum.slice(x, w) |> MapSet.new
-    if MapSet.size(s) == w, do: x + w, else: solve(i, x + 1, w)
+    s = i |> Enum.slice(x, w) |> MapSet.new |> MapSet.size
+    if s== 0 || s == w, do: x + w, else: solve(i, x + 1, w)
   end
 end
